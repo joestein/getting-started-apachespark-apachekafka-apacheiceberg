03e102c4a278a6767b657f16a453e0a1f633e5b8
Joe Stein <cryptcom@gmail.com>
2023-11-19 16:47:59 -0500

    some more fixes from testing

--- create_table.py
+++ create_table.py
@@ -42,14 +42,13 @@ spark._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3
 spark._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider", "com.amazonaws.auth.InstanceProfileCredentialsProvider,com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
 spark._jsc.hadoopConfiguration().set("fs.AbstractFileSystem.s3a.impl", "org.apache.hadoop.fs.s3a.S3A")
 
-#spark.sql(f"""
-#  CREATE TABLE nessie.{table_name} (
-#          email STRING, 
-#          firstName STRING, 
-#          lastName STRING
-#          ) 
-#          USING iceberg
-#          LOCATION 's3a://{catalog}/{table_name}'
-#""")
+spark.sql(f"""
+  CREATE TABLE nessie.{table_name} (
+          email STRING, 
+          firstName STRING, 
+          lastName STRING
+          ) 
+          USING iceberg
+          LOCATION 's3a://{catalog}/{table_name}'
+""")
 
-spark.sql("select * from nessie.yoyoma4").show()
